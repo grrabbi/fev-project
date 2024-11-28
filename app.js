aaf6feed75486f042667a7b7683ab09153822e71
Golam Rabbi <rabbishah966@gmail.com>
2024-11-29 00:38:35 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+particlesJS('particles-js',
+
+    {
+  "particles": {
+    "number": {
+      "value": 173,
+      "density": {
+        "enable": false,
+        "value_area": 960
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.536,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2.1877050973528784,
+        "opacity_min": 0.1782574523769012,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 24.307834415031987,
+        "size_min": 3.2410445886709307,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 160,
+      "color": "#ffffff",
+      "opacity": 0.376,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+);
